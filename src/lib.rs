e7cbcce787a6c1e70f9a1b14a82d79f4ff7472ed
Zeyla Hellyer <hi@zeyla.me>
2018-10-31 15:15:01 -0700

    Add tests for duplicate keys

--- src/lib.rs
+++ src/lib.rs
@@ -10,15 +10,43 @@ struct List {
 }
 
 pub fn all() -> Result<HashMap<String, String>, TomlDeError> {
-    let list = toml::de::from_str::<List>(include_str!("../list.toml"))?;
+    let list = toml::from_slice::<List>(include_bytes!("../list.toml"))?;
 
     Ok(list.radio_stations)
 }
 
 #[cfg(test)]
 mod tests {
+    use std::collections::HashMap;
+    use toml::Value;
+
     #[test]
     fn test_deser() {
         assert!(super::all().is_ok());
+    }
+
+    // `toml` has a bug[1] where deserializing to a non-Value ignores
+    // duplicates.
+    //
+    // So, we need to deserialize to a Value here so that we can check that we
+    // have none.
+    //
+    // Refer to the next test for verification of this.
+    //
+    // [1]: https://github.com/alexcrichton/toml-rs/issues/230
+    #[test]
+    fn test_duplicates() {
+        assert!(toml::from_slice::<Value>(include_bytes!("../list.toml")).is_ok());
+    }
+
+    // Showcase that deserializing to a `Value` from a string with duplicates
+    // returns an error, but deserializing to a `HashMap<String, u64>` does not
+    // error.
+    #[test]
+    fn test_duplicates_error() {
+        const INPUT: &str = "a = 1\nb = 2\na = 3";
+
+        assert!(toml::from_str::<Value>(INPUT).is_err());
+        assert!(toml::from_str::<HashMap<String, u64>>(INPUT).is_ok());
     }
 }
